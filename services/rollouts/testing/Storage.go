4f562c21fb44532fa75db32651ecd6edf89eb4a8
Adam Luzsi <adamluzsi@gmail.com>
2019-04-23 22:54:19 +0200

    create testing which will help with testing both interactors and usecases for rollout functionalities

---
+++ services/rollouts/testing/Storage.go
@@ -0,0 +1,60 @@
+package testing
+
+import (
+	"github.com/adamluzsi/FeatureFlags/services/rollouts"
+	"github.com/adamluzsi/frameless"
+	"github.com/adamluzsi/frameless/iterators"
+	"github.com/adamluzsi/frameless/resources/storages/memorystorage"
+)
+
+func NewStorage() *Storage {
+	return &Storage{Memory: memorystorage.NewMemory()}
+}
+
+type Storage struct{ *memorystorage.Memory }
+
+func (storage *Storage) FindPilotsByFeatureFlag(ff *rollouts.FeatureFlag) frameless.Iterator {
+	table := storage.TableFor(rollouts.Pilot{})
+
+	var pilots []*rollouts.Pilot
+
+	for _, v := range table {
+		pilot := v.(*rollouts.Pilot)
+
+		if pilot.FeatureFlagID == ff.ID {
+			pilots = append(pilots, pilot)
+		}
+	}
+
+	return iterators.NewSlice(pilots)
+}
+
+func (storage *Storage) FindFlagPilotByExternalPilotID(FeatureFlagID, ExternalPublicPilotID string) (*rollouts.Pilot, error) {
+	table := storage.TableFor(rollouts.Pilot{})
+
+	for _, v := range table {
+		pilot := v.(*rollouts.Pilot)
+
+		if pilot.FeatureFlagID == FeatureFlagID && pilot.ExternalID == ExternalPublicPilotID {
+			return pilot, nil
+		}
+	}
+
+	return nil, nil
+}
+
+func (storage *Storage) FindByFlagName(name string) (*rollouts.FeatureFlag, error) {
+	var ptr *rollouts.FeatureFlag
+	table := storage.TableFor(ptr)
+
+	for _, v := range table {
+		flag := v.(*rollouts.FeatureFlag)
+
+		if flag.Name == name {
+			ptr = flag
+			break
+		}
+	}
+
+	return ptr, nil
+}
