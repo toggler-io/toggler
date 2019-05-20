f616052da1a43746595a86a9f1ff70d52de99b49
Adam Luzsi <adamluzsi@gmail.com>
2019-05-20 23:57:04 +0200

    enforce token for feature flag rollout percentage update

--- usecases/UpdateFeatureFlagRolloutPercentage.go
+++ usecases/UpdateFeatureFlagRolloutPercentage.go
@@ -1,5 +1,17 @@
 package usecases
 
-func (uc *UseCases) UpdateFeatureFlagRolloutPercentage(featureFlagName string, rolloutPercentage int) error {
+func (uc *UseCases) UpdateFeatureFlagRolloutPercentage(token string, featureFlagName string, rolloutPercentage int) error {
+
+	valid, err := uc.Doorkeeper.VerifyTokenString(token)
+
+	if err != nil {
+		return err
+	}
+
+	if !valid {
+		return ErrInvalidToken
+	}
+
 	return uc.RolloutManager.UpdateFeatureFlagRolloutPercentage(featureFlagName, rolloutPercentage)
+
 }
--- usecases/UpdateFeatureFlagRolloutPercentage_test.go
+++ usecases/UpdateFeatureFlagRolloutPercentage_test.go
@@ -1,1 +1,50 @@
-package usecases
+package usecases_test
+
+import (
+	"github.com/adamluzsi/FeatureFlags/usecases"
+	"github.com/adamluzsi/testcase"
+	"github.com/stretchr/testify/require"
+	"math/rand"
+	"testing"
+	. "github.com/adamluzsi/FeatureFlags/testing"
+)
+
+func TestUseCases_UpdateFeatureFlagRolloutPercentage(t *testing.T) {
+	s := testcase.NewSpec(t)
+	SetupSpecCommonVariables(s)
+	SetupSpec(s)
+	s.Parallel()
+
+	subject := func(t *testcase.T) error {
+		return GetUseCases(t).UpdateFeatureFlagRolloutPercentage(
+			t.I(`TokenString`).(string),
+			GetFeatureFlagName(t),
+			GetRolloutPercentage(t),
+		)
+	}
+
+	s.Let(`RolloutPercentage`, func(t *testcase.T) interface{} { return rand.Intn(128) + 1 })
+
+	s.When(`caller have invalid token`, func(s *testcase.Spec) {
+		s.Let(`TokenString`, func(t *testcase.T) interface{} { return `invalid token` })
+
+		s.Then(`it will be rejected`, func(t *testcase.T) {
+			require.Equal(t, usecases.ErrInvalidToken, subject(t))
+		})
+	})
+
+	s.When(`caller have a valid token`, func(s *testcase.Spec) {
+		s.Let(`TokenString`, func(t *testcase.T) interface{} {
+			return CreateToken(t, `rollout manager uniq id`).Token
+		})
+
+		s.Then(`it will set percentage`, func(t *testcase.T) {
+			require.Nil(t, subject(t))
+			flag, err := GetStorage(t).FindFlagByName(GetFeatureFlagName(t))
+			require.Nil(t, err)
+			require.NotNil(t, flag)
+			require.Equal(t, GetRolloutPercentage(t), flag.Rollout.Strategy.Percentage)
+		})
+	})
+
+}
