66c9c4d5e17ec6a3e439b678be4514d9e113194b
Adam Luzsi <adamluzsi@gmail.com>
2019-06-25 03:35:10 +0200

    add postgres url schema as a valid pg connection string schema

--- extintf/storages/New.go
+++ extintf/storages/New.go
@@ -1,1 +1,28 @@
 package storages
+
+import (
+	"database/sql"
+	"github.com/adamluzsi/frameless"
+	"github.com/adamluzsi/toggler/extintf/storages/postgres"
+	"github.com/adamluzsi/toggler/usecases"
+	"net/url"
+)
+
+func New(connstr string) (usecases.Storage, error) {
+	u, err := url.Parse(connstr)
+	if err != nil {
+		return nil, err
+	}
+
+	switch u.Scheme {
+	case `postgres`:
+		db, err := sql.Open(u.Scheme, connstr)
+		if err != nil {
+			return nil, err
+		}
+		return postgres.NewPostgres(db)
+
+	default:
+		return nil, frameless.ErrNotImplemented
+	}
+}
--- extintf/storages/New_test.go
+++ extintf/storages/New_test.go
@@ -1,1 +1,62 @@
-package storages
+package storages_test
+
+import (
+	"fmt"
+	"github.com/adamluzsi/testcase"
+	"github.com/adamluzsi/toggler/extintf/storages"
+	"github.com/adamluzsi/toggler/extintf/storages/postgres"
+	"github.com/adamluzsi/toggler/usecases"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	s := testcase.NewSpec(t)
+
+	s.Describe(`New`, func(s *testcase.Spec) {
+		subject := func(t *testcase.T) (usecases.Storage, error) {
+			return storages.New(t.I(`connstr`).(string))
+		}
+
+		onSuccess := func(t *testcase.T) (usecases.Storage) {
+			s, err := subject(t)
+			require.Nil(t, err)
+			return s
+		}
+
+		s.When(`the connection string is unknown`, func(s *testcase.Spec) {
+			s.Let(`connstr`, func(t *testcase.T) interface{} {
+				return `nexthypedstoragesystem://user:pwd@localhost:8100/db`
+			})
+
+			s.Then(`it will result in error`, func(t *testcase.T) {
+				s, err := subject(t)
+				require.Nil(t, s)
+				require.Error(t, err)
+			})
+		})
+
+		s.When(`the connection string is a postgres db`, func(s *testcase.Spec) {
+			s.Let(`connstr`, func(t *testcase.T) interface{} {
+				return fmt.Sprintf(`%s://postgres@localhost:8100/postgres?sslmode=disable`, t.I(`driver name`).(string))
+			})
+
+			for _, driverName := range []string{
+				`postgres`,
+			} {
+				s.And(fmt.Sprintf(`with the driver name is %s`, driverName), func(s *testcase.Spec) {
+					driverName := driverName // pass by value copy to remove loop variable dependency in the callbacks
+					s.Let(`driver name`, func(t *testcase.T) interface{} { return driverName })
+
+					s.Then(`then it will return postgres implementation`, func(t *testcase.T) {
+						_, isPG := onSuccess(t).(*postgres.Postgres)
+
+						require.True(t, isPG)
+					})
+
+				})
+			}
+
+		})
+	})
+}
