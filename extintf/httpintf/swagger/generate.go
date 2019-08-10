8f4668eb4c5a32db7261bdd28ea5e43eca9abb66
Adam Luzsi <adamluzsi@gmail.com>
2019-08-11 00:52:22 +0200

    fmt

--- extintf/httpintf/swagger/generate.go
+++ extintf/httpintf/swagger/generate.go
@@ -2,6 +2,6 @@ package swagger
 
 //go:generate swagger generate spec -b .. -o swagger.json
 //go:generate swagger validate swagger.json
-//go:generate esc -o ./specfs/fs.go -pkg specfs swagger.json
 //go:generate swagger generate client --quiet --spec swagger.json --target ${WDP}/lib/go
+//go:generate esc -o ./specfs/fs.go -pkg specfs swagger.json
 //go:generate esc -o ./uifs/fs.go -pkg uifs -prefix assets/swagger-ui ./assets
