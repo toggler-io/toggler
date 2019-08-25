d1e457823d6f7858d2694ffac1365bc21e4a4657
Adam Luzsi <adamluzsi@gmail.com>
2019-08-26 01:32:37 +0200

    fix swagger assets generation order bug, to ensure generated fs is present to make code valid during swagger specification generation

--- extintf/httpintf/swagger/generate.go
+++ extintf/httpintf/swagger/generate.go
@@ -1,7 +1,8 @@
 package swagger
 
+//go:generate esc -o ./specfs/fs.go -pkg specfs swagger.json
+//go:generate esc -o ./uifs/fs.go -pkg uifs -prefix assets/swagger-ui ./assets
+
 //go:generate swagger generate spec -b .. -o swagger.json
 //go:generate swagger validate swagger.json
 //go:generate swagger generate client --quiet --spec swagger.json --target ${WDP}/lib/go
-//go:generate esc -o ./specfs/fs.go -pkg specfs swagger.json
-//go:generate esc -o ./uifs/fs.go -pkg uifs -prefix assets/swagger-ui ./assets
