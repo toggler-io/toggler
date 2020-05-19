8ac9eac390ffd4275f4e34b98f56a8dd0e86695d
Adam Luzsi <adamluzsi@gmail.com>
2020-05-19 04:04:40 +0200

    update tools.go file

--- tools.go
+++ tools.go
@@ -1,12 +1,13 @@
 // +build tools
 
+//go:generate mkdir -p .tools
 package toggler
 
-//go:generate go install github.com/golang/mock/mockgen
-//go:generate go install github.com/go-swagger/go-swagger/cmd/swagger
+//go:generate go build -o .tools/ github.com/go-swagger/go-swagger/cmd/swagger
+import _ "github.com/go-swagger/go-swagger"
 
-import (
-	_ "github.com/go-swagger/go-swagger"
-	_ "github.com/golang/mock/gomock"
-	_ "github.com/mjibson/esc"
-)
+//go:generate go build -o .tools/ github.com/golang/mock/mockgen
+import _ "github.com/golang/mock/gomock"
+
+//go:generate go build -o .tools/ github.com/mjibson/esc
+import _ "github.com/mjibson/esc"
